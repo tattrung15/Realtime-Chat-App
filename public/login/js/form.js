db238e9ccf9b29a3a3172588c002e9d4b932a900
Tất Trung <55653291+tattrung15@users.noreply.github.com>
2020-05-05 14:18:30 +0700

    a

--- public/login/js/form.js
+++ public/login/js/form.js
@@ -1,5 +1,3 @@
-!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/8fa7a38a54cc69f68e5be0b75/b7ac124db40290e30195476e8.js");
-
 function validate(){
     let regexUsername = /^[a-zA-Z0-9_.@-]{6,20}$/;
     let regexPassword = /^[a-zA-Z0-9_.@-]{6,20}$/;
