af207508f7332f901bbd70e6260cbe8e504c626d
Tất Trung <55653291+tattrung15@users.noreply.github.com>
2020-06-05 13:49:43 +0700

    secure api

--- API/controller/user.controller.js
+++ API/controller/user.controller.js
@@ -1,18 +1,33 @@
-const mongoose = require('mongoose');
-
+//require('dotenv').config();
 const User = require('../../model/userModel');
 
 module.exports.allUser = async (req, res) => {
 
     try {
-        let data = await User.find();
+        let key = req.body.name;
 
-        res.status(200).json({
-            message: 'success',
-            data: data
-        });
+        if(!key){
+            res.status(500).json({
+                message: 'Unauthorization'
+            });
+            return;
+        } else {
+            if(key === process.env.KEY){
+                let data = await User.find();
+
+                res.status(200).json({
+                    message: 'success',
+                    data: data
+                });
+            } else {
+                res.status(500).json({
+                    message: 'Unauthorization'
+                });
+                return;
+            }
+        }
     } catch (error) {
-        res.status(400).json({
+        res.status(500).json({
             message: 'fail',
             error: error
         });
--- API/router/user.route.js
+++ API/router/user.route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const userController = require('../controller/user.controller');
 
-router.get('/v1/user', userController.allUser);
+router.post('/v1/user', userController.allUser);
 
 module.exports = router;
